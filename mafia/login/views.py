8755362549ce0079112a3e187633363617eaeb2b
Nathan Chiu <nathan.chiu94@gmail.com>
2020-05-06 17:07:47 -0700

    Change messaging system to use Django request messages. Update style.css in login app to work with errors and message listing.

--- mafia/login/views.py
+++ mafia/login/views.py
@@ -11,16 +11,15 @@ from django.contrib.auth import authenticate, update_session_auth_hash
 from django.contrib.auth import login as django_login
 from django.contrib.auth import logout as django_logout
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, AuthenticationForm
-# Create your views here.
+
 
 def getErrorMessages(errors_as_data):
+    #return all error messages in a single list as strings.
     err_messages = []
     for val in errors_as_data.values():
         err_messages += val[0]
     return err_messages
 
-class IndexView(auth_views.LoginView):
-    template_name = 'login/index.html'
 
 def index(request):
     if request.method == 'POST':
@@ -33,17 +32,19 @@ def index(request):
             return redirect('/login/')
         else:
             err_messages = getErrorMessages(form.errors.as_data())
-            return render(request, 'login/index.html', {'form': form, 'err_messages': err_messages})
+            for message in err_messages:
+                messages.error(request, message)
+            return render(request, 'login/index.html', {'form': form})
     else:
         form = AuthenticationForm()
-        user = request.user
-        return render(request, 'login/index.html', {'form': form, 'user': user})
+        return render(request, 'login/index.html', {'form': form})
 
-        # return redirect('/login/')
+
 def logout(request):
     if request.user.is_authenticated:
         django_logout(request)
     return redirect('/login/')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -57,25 +58,29 @@ def signup(request):
             return redirect('/login/')
         else:
             err_messages = getErrorMessages(form.errors.as_data())
-            return render(request, 'login/signup.html', {'form': form, 'err_messages': err_messages})
+            for message in err_messages:
+                messages.error(request, message)
+            return render(request, 'login/signup.html', {'form': form})
     else:
         form = UserCreationForm()
-        user = request.user
-    return render(request, 'login/signup.html', {'form': form, 'user':user})
+    return render(request, 'login/signup.html', {'form': form})
+
 
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
+            messages.success(request, 'Your password was successfully updated.')
             #log the user in
             update_session_auth_hash(request, user)
-            # messages.success(request, 'Your password was successfully updated.')
-            messages = ['Your password has been successfully changed.', 'You have been logged in again with your new credentials.']
-            return render(request, 'login/change_password.html', {'form': form, 'messages': messages})
+            messages.info(request, 'You have been logged in with your new credentials.')
+            return redirect('/login/')
         else:
             err_messages = getErrorMessages(form.errors.as_data())
-            return render(request, 'login/change_password.html', {'form': form, 'err_messages': err_messages})
+            for message in err_messages:
+                messages.error(request, message)
+            return render(request, 'login/change_password.html', {'form': form})
     else:
         form = PasswordChangeForm(request.user)
     return render(request, 'login/change_password.html', {'form':form})
